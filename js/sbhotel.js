faf3cace5a077cbb5135edea0ce2a02dec88a7f4
Eli <hellan_nikolova@abv.bg>
2015-07-25 14:09:16 +0300

    Add section About Kiten.

--- js/sbhotel.js
+++ js/sbhotel.js
@@ -8,7 +8,23 @@ $(document).ready(function() {
     showDesc(".room1 p");
     showDesc(".room2 p");
     showDesc(".room3 p");
+    
 
 });
 
 
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1400);
+                return false;
+            }
+        }
+    });
+});
